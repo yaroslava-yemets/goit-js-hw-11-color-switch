d96b533b1cc00f24157daf39144729c1c2652001
Yaroslava Yemets <yaroslava1058@gmail.com>
2021-06-16 13:57:11 +0300

    created colored-switch

---
+++ index.js
@@ -0,0 +1,42 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+
+  const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const refs = {
+      startBtn: document.querySelector('[data-action="start"]'),
+      stopBtn: document.querySelector('[data-action="stop"]'),
+      body: document.querySelector('body'),
+  };
+
+  let intervalId = null;
+  let btnIsActive = false;
+
+  refs.startBtn.addEventListener('click', onStartClick);
+  refs.stopBtn.addEventListener('click', onStopClick);
+
+  function onStartClick () {
+      if(btnIsActive){
+          return;
+      };
+
+      intervalId = setInterval(() => {
+          btnIsActive = true;
+          const randomIntegerNumber = randomIntegerFromInterval(0, colors.length - 1);
+          refs.body.style.backgroundColor = colors[randomIntegerNumber];
+      }, 15);
+  };
+
+  function onStopClick () {
+    btnIsActive = false;
+    clearInterval(intervalId);
+  };
+
